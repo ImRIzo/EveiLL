5f3a6731db698d1e209ae5bced1fc3b9487ee78c
Rizwan <rizowan69@gmail.com>
2020-12-28 13:13:44 +0600

    ll
    last

---
+++ EveiLL/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = .1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
